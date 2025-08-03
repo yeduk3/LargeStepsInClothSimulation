56f37b6c7bdb830d43aed64870826e2076a31938
yeduk3 <yeduk33@gmail.com>
2025-08-04 00:58:42 +0900

    Initial Commit

---
+++ LargeStepInClothSimulationXCode/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LargeStepInClothSimulationXCode
+//
+//  Created by 이용규 on 8/4/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
